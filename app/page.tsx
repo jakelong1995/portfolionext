ebf8c370e2badc537dfb8bf290d58f9dd3f6e5d8
jakelong1995 <hoanglong2579@gmail.com>
2024-02-16 16:54:32 +0700

    project+intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">{/* <Intro /> */}</main>
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <Projects />
+    </main>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,48 +4,51 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
-import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { FiArrowRight, FiDownload } from "react-icons/fi";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import profile from "@/public/profile.png";
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src={profile}
-              alt="Ricardo portrait"
-              priority={true}
-              className="w-1/4 rounded-xl object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
-        </div>
+    <section className="flex-col max-w-3xl flex gap-8">
+      <div className="flex flex-col md:flex-row items-center h-fit px-4 py-8 justify-between mx-auto sm:gap-8">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "tween",
+            duration: 0.2,
+          }}
+        >
+          <Image
+            src={profile}
+            alt="profilepic"
+            priority={true}
+            className="rounded-full object-cover w-48 h-fit border-[0.3rem] border-white shadow-lg"
+          />
+        </motion.div>
+        <motion.div
+          className="w-full flex-col flex gap-4 text-gray-700 text-center max-w-md sm:text-left !leading-[1.5]"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <h1 className="text-3xl pt-6 sm:pt-0">
+            Hi I'm <span className="font-semibold">Hoang Long!</span>
+          </h1>
+
+          <p className="text-xl !leading-[1.5] ">
+            <span className="font-semibold">
+              UX Designer & full-stack developer
+            </span>{" "}
+            with <span className="font-semibold">4 years</span> of experience. I
+            enjoy building{" "}
+            <span className="italic">usable & aesthetic websites</span>. My
+            focus is <span className="underline">React (Next.js)</span>.
+          </p>
+        </motion.div>
       </div>
-
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
-      </motion.h1>
-
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -54,40 +57,39 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className=" bg-gray-800 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-900 active:scale-105 transition shadow-sm"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
-          Contact me here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          Contact me <FiArrowRight className="opacity-80 " />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className=" bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 shadow-sm"
           href="/CV.pdf"
           download
         >
-          Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          Download CV <FiDownload className="opacity-80 " />
         </a>
+        <div className="flex gap-4">
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 shadow-sm"
+            href="https://www.linkedin.com/in/jakelong1995/"
+            target="_blank"
+          >
+            <FaLinkedinIn />
+          </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 shadow-sm"
+            href="https://github.com/jakelong1995"
+            target="_blank"
+          >
+            <FaGithub />
+          </a>
+        </div>
       </motion.div>
     </section>
   );
