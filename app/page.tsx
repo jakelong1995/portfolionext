6c7ec520ce342e6fc67a5f20f0ad64514c149d30
jakelong1995 <hoanglong2579@gmail.com>
2024-05-06 15:38:30 +0700

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-import Projects from "@/components/projectss";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
