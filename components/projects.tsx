6e98c5e5455b6c82a0c6db9747ddce08ccd95330
jakelong1995 <hoanglong2579@gmail.com>
2024-05-13 12:18:24 +0700

    update

--- components/projects.tsx
+++ components/projects.tsx
@@ -23,7 +23,7 @@ export default function Projects() {
 
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8 h-auto">
           {projectsData.map((project, index) => (
-            <motion.div
+            <motion.li
               variants={fadeInAnimationVariants}
               initial="initial"
               whileInView="animate"
@@ -67,7 +67,7 @@ export default function Projects() {
                   </CardHeader>
                 </Card>
               </Link>
-            </motion.div>
+            </motion.li>
           ))}
         </ul>
       </div>
