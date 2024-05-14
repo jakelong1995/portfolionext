6f53deeff3de17ad5acd8c45d7f136375e9fe693
jakelong1995 <hoanglong2579@gmail.com>
2024-05-14 16:27:35 +0700

    header fixed

--- components/header.tsx
+++ components/header.tsx
@@ -13,53 +13,51 @@ export default function Header() {
 
   return (
     <header className="z-[999] relative">
-      <motion.div
-        className="fixed flex p-1 top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-fit sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+      <motion.nav
+        className="fixed p-1 top-0 left-1/2 border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       >
-        <nav className="h-full">
-          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-            {links.map((link) => (
-              <motion.li
-                className="h-3/4 flex items-center justify-center relative"
-                key={link.hash}
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
+        <ul className="flex items-center justify-center text-[0.9rem] font-medium text-gray-500 gap-4">
+          {links.map((link) => (
+            <motion.li
+              className="flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex items-center justify-center px-4 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
               >
-                <Link
-                  className={clsx(
-                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
-                    {
-                      "text-gray-950 dark:text-gray-200":
-                        activeSection === link.name,
-                    }
-                  )}
-                  href={link.hash}
-                  onClick={() => {
-                    setActiveSection(link.name);
-                    setTimeOfLastClick(Date.now());
-                  }}
-                >
-                  {link.name}
+                {link.name}
 
-                  {link.name === activeSection && (
-                    <motion.span
-                      className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                      layoutId="activeSection"
-                      transition={{
-                        type: "spring",
-                        stiffness: 380,
-                        damping: 30,
-                      }}
-                    ></motion.span>
-                  )}
-                </Link>
-              </motion.li>
-            ))}
-          </ul>
-        </nav>
-      </motion.div>
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </motion.nav>
     </header>
   );
 }
