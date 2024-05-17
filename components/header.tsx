a763c2ba7cc50c87beab27d0be72eb344af8f6e9
jakelong1995 <hoanglong2579@gmail.com>
2024-05-17 13:57:59 +0700

    update header

--- components/header.tsx
+++ components/header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
                 className={clsx(
                   "flex items-center justify-center px-4 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-950 dark:text-gray-100":
                       activeSection === link.name,
                   }
                 )}
