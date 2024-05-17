7f8ad4e54ba3d5a1f5e59c17868c2ece401cc6c9
jakelong1995 <hoanglong2579@gmail.com>
2024-05-17 17:21:05 +0700

    fix scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/context/theme-provider";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} relative pt-28 sm:pt-36`}>
         {" "}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
--- components/contact.tsx
+++ components/contact.tsx
@@ -6,14 +6,51 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import SectionHeading from "@/components/section-heading";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+import { sendEmail } from "@/actions/sendEmail";
+import { useToast } from "@/components/ui/use-toast";
+import { Send } from "lucide-react";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact", 0.5);
+  const { toast } = useToast();
+
+  const handleSubmit = async (formData: FormData) => {
+    const { data, error } = await sendEmail(formData);
+
+    if (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error,
+      });
+      return;
+    }
+
+    toast({
+      variant: "default",
+      title: "Success",
+      description: "Email sent successfully!",
+    });
+  };
+
   return (
-    <section
+    <motion.section
       ref={ref}
       id="contact"
       className="mb-28 max-w-3xl scroll-mt-28 px-4 md:px-6 space-y-6"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <div className="text-center">
         <SectionHeading>Get in touch</SectionHeading>
@@ -22,12 +59,20 @@ export default function Contact() {
           we'll get back to you as soon as possible.
         </p>
       </div>
-      <form className="space-y-4">
+      <form
+        className="space-y-4"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          await handleSubmit(formData);
+        }}
+      >
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
           <div className="space-y-1.5">
             <Label htmlFor="name">Name</Label>
             <Input
               id="name"
+              name="name"
               placeholder="Enter your name"
               className="dark:bg-gray-900"
             />
@@ -36,6 +81,7 @@ export default function Contact() {
             <Label htmlFor="email">Email</Label>
             <Input
               id="email"
+              name="email"
               placeholder="Enter your email"
               type="email"
               className="dark:bg-gray-900"
@@ -46,6 +92,7 @@ export default function Contact() {
           <Label htmlFor="message">Message</Label>
           <Textarea
             id="message"
+            name="message"
             placeholder="Enter your message"
             rows={5}
             className="dark:bg-gray-900"
@@ -53,8 +100,9 @@ export default function Contact() {
         </div>
         <Button className="w-full rounded-full h-12" type="submit">
           Submit
+          <Send className="ml-2 h-4 w-4" />
         </Button>
       </form>
-    </section>
+    </motion.section>
   );
 }
