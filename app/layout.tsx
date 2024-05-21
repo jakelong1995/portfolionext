9272542f7d3339647ef3a2d98b7c213d12495f8d
jakelong1995 <hoanglong2579@gmail.com>
2024-05-21 11:03:09 +0700

    header mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/header copy";
 import { ThemeProvider } from "@/context/theme-provider";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
